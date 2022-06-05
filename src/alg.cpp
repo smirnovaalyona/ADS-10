dba7b93fd062d6983ee3ab4ce9fcef13d5d71f00
smirnovaalyona <99326020+smirnovaalyona@users.noreply.github.com>
2022-06-05 23:07:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,10 @@
 
 std::vector<char> getPerm(const Tree& tree, int n) {
   // напишите реализацию
+  std::string sour = tree[n - 1];
+    std::vector<char> tree1;
+    for (int i = 0; i < sour.length(); i++) {
+        tree1.push_back(sour[i]);
+    }
+    return tree1;
 }
